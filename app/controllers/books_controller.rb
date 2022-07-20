90bae352741a19868f41b1af1a3f91632af92cad
masaki08 <108650138+masaki08@users.noreply.github.com>
2022-07-20 11:55:17 +0000

    first-commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,10 +11,13 @@ class BooksController < ApplicationController
  end
 
  def update
-  book = Book.find(params[:id])
-  book.update(book_params)
-  
-  redirect_to book_path(book.id)
+   @book = Book.find(params[:id])
+  if @book.update(book_params)
+   flash[:success] = "successfully"
+  redirect_to book_path(@book.id)
+  else
+   render :edit
+  end
  end
 
  def destroy
@@ -29,10 +32,11 @@ class BooksController < ApplicationController
 
  def create
     @book = Book.new(book_params)
-   if @book.save
-     redirect_to book_path(book.id)
+  if @book.save
+    flash[:success] = "successfully"
+     redirect_to book_path(@book.id)
   else
-     @book = Book.all
+     @books = Book.all
      render :index
   end
  end
